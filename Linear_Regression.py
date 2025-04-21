c94b69a9c94576be1c1b177cb75cf7f74d0a7779
şeref can memiş <canmemis@marun.edu.tr>
2025-04-21 23:23:10 +0300

    gradient

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -22,16 +22,26 @@ def R2_score(y,predictions):
     return R2
 
 class LinearRegression():
-    def __init__(self):
+    def __init__(self,method = 'normal',epoch = 300):
         self.weights = None
+        self.method = method
+        self.epoch = epoch
         
     def fit(self,X,y):
-        X['intercept'] = 1
-        cols = ['intercept'] + [col for col in X.columns if col != 'intercept']
-        X = X[cols]
+        n_samples,n_features = X.shape
+        X = np.hstack(np.ones((n_samples,1)),X)
         X_T = X.T
-        B = np.linalg.inv(X_T @ X) @ X_T @ y
-        self.weights = B
+        if self.method == 'normal':
+            B = np.linalg.inv(X_T @ X) @ X_T @ y
+            self.weights = B
+        if self.method == 'gradient':
+            y_values = y.values
+            X_values = X.values
+            
+            self.weights = np.zeros(n_features,1)   
+            for _ in self.epoch:
+                
+            
 
     def predict(self,X):
         X['intercept'] = 1
@@ -44,7 +54,7 @@ class LinearRegression():
 x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.2)
 print(x_train.shape)
 print(x_test.shape)
-model = LinearRegression()
+model = LinearRegression(method='gradient')
 model.fit(x_train,y_train)
 preds = model.predict(x_test)
 R2_score(y_test,preds)
@@ -53,10 +63,15 @@ mean_squared_error(y_test,preds)
 
 
 
-
-
-
-
+y = y.values
+X = X.values
+n_samples,n_features = X.shape
+weights = np.zeros((n_features,1))   
+for _ in 100:
+    predictions = X @ weights
+    error = (y - predictions)
+    deltaM = error.T @ X
+    weights = deltaM * 0.01
 
 
 
